fe3025cbf2b522e89693952d66f78d732bcccbcf
kwamekute <thatkutekwame@gmail.com>
2023-11-01 13:25:50 +0000

    completed

--- rock.js
+++ rock.js
@@ -1,8 +1,9 @@
 let playerScore = 0;
 let computerScore = 0;
+let roundVerdict = "";
 
 
-let getComputerChoice = function () {
+const getComputerChoice = function () {
     const options = ['rock', 'paper', 'scissors'];
 
     //select a random index from the options string above
@@ -14,42 +15,81 @@ let getComputerChoice = function () {
     return randomChoice;
 };
 
-let getUserChoice = function () {
-    return prompt("Enter your choice: ");
-};
 
-const playRound = function () {
-    let userSelection = getUserChoice();
+const playRound = function (userChoice) {
+    let userSelection = userChoice;
 
     let computerSelection = getComputerChoice();
 
     if (userSelection === computerSelection) {
-        alert(`It's a tie you chose ${userSelection} computer chose ${computerSelection} `);
+        roundVerdict = `It's a tie you chose ${userSelection} computer chose ${computerSelection} `;
 
         return playerScore += 1;
     }
     else if (userSelection === "rock" && computerSelection === "paper") {
 
-        alert(`You lose. You chose ${userSelection} computer chose ${computerSelection} `);
+        roundVerdict = `You lose. You chose ${userSelection} computer chose ${computerSelection} `;
 
         return computerScore += 1;
 
     }
     else if (userSelection === "paper" && computerSelection === "scissors") {
 
-        alert(`You lose. You chose ${userSelection} computer chose ${computerSelection} `);
+        roundVerdict = `You lose. You chose ${userSelection} computer chose ${computerSelection} `;
 
         return computerScore += 1;
 
 
     }
     else if (userSelection === "scissors" && computerSelection === "rock") {
-        alert(`You lose. You chose ${userSelection} computer chose ${computerSelection} `);
+        roundVerdict = `You lose. You chose ${userSelection} computer chose ${computerSelection} `;
 
         return computerScore = + 1;
     };
 
 };
+
+//get the choice
+const choices = document.querySelectorAll('.action');
+
+choices.forEach(function (choice) {
+    choice.addEventListener('click', () => {
+        const clickedOption = choice.innerText.toLowerCase();
+        playRound(clickedOption);
+
+        const playerScores = document.querySelector('#playerScores');
+        const computerScores = document.querySelector('#computerScores');
+        const verdict = document.querySelector('#result');
+
+        playerScores.innerText = playerScore;
+        computerScores.innerText = computerScore;
+        verdict.innerText = roundVerdict;
+
+        if (playerScore == 5 || computerScore == 5) {
+            if (playerScore > computerScore) {
+                verdict.innerText = "You won!!!"
+                verdict.setAttribute("style", "color: green");
+            }
+            else {
+                verdict.innerText = "You lost!!!"
+                verdict.setAttribute("style", "color: red");
+            }
+
+        }
+    });
+})
+
+
+
+
+const resetBtn = document.querySelector("#reset");
+
+resetBtn.addEventListener('click', () => {
+    location.reload();
+})
+
+
+
 
 // for (let i = 0; i < 6; i++) {
 //     playRound();
